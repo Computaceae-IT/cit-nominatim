d3a74265dd5f9fca0ef9786ef313ad2ed88ce7d2
SiropOps <cyril.boillat@ville-ge.ch>
2021-04-21 17:15:51 +0200

    feat. (USER) change

--- startpostgres.sh
+++ startpostgres.sh
@@ -4,6 +4,6 @@ service postgresql start
 
 # for update
 touch /var/log/update.log
-nohup sudo -u nominatim ./src/build/utils/update.php --import-osmosis-all > /var/log/update.log  2>&1&
+nohup sudo -u postgres ./src/build/utils/update.php --import-osmosis-all > /var/log/update.log  2>&1&
 
 tail -f /var/log/postgresql/postgresql-12-main.log -f /var/log/update.log
