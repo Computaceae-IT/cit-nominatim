0f25d777b469f19a7c8cb5432cb00d05c612ec8b
SiropOps <cyril@botalista.community>
2025-05-20 20:03:13 +0200

    redo

--- DB/conf.d/local.php
+++ DB/conf.d/local.php
@@ -9,4 +9,4 @@
  @define('CONST_Replication_Update_Interval', '86400');  // How often upstream publishes diffs
  @define('CONST_Replication_Recheck_Interval', '900');   // How long to sleep if no update found yet
  @define('CONST_Pyosmium_Binary', '/usr/local/bin/pyosmium-get-changes');
- // @define('CONST_Database_DSN', 'pgsql:host=172.16.0.1;port=6432;user=nominatim;password=99c522b62516f1aab9b7a7a7f058078b;dbname=nominatim'); <driver>:host=<host>;port=<port>;user=<username>;password=<password>;dbname=<database>
+ // @define('CONST_Database_DSN', 'pgsql:host=172.16.0.250;port=6432;user=nominatim;password=99c522b62516f1aab9b7a7a7f058078b;dbname=nominatim'); <driver>:host=<host>;port=<port>;user=<username>;password=<password>;dbname=<database>
